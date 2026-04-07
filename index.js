7ba2f24ccb1d6ecf1040d3db0283920be540132e
PAI-Peganov <artyompeganov428@gmail.com>
2026-04-07 18:48:50 +0500

    pai3

--- index.js
+++ index.js
@@ -5,3 +5,14 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+const btnClk = document.getElementById("ModalOpenBtn");
+btnClk.addEventListener("click", () => {
+    const bluer = document.getElementById("blur");
+    bluer.style.display = "block";
+})
+
+const btnCross = document.getElementById("modal1_cross");
+btnCross.addEventListener("click", () => {
+    const bluer = document.getElementById("blur");
+    bluer.style.display = "none";
+})
